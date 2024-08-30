905e1842f3f58fa84020c10b9fd74130d2a98aaf
AliAhmed015 <114981587+AliAhmed015@users.noreply.github.com>
2024-08-30 23:01:33 +0500

    Update About.jsx

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -16,27 +16,26 @@ import React from "react";
  * Need an image? Check out https://unsplash.com to download a image you
  * freely use on your site.
  */
-import image from "../images/motion-background.jpg";
+import image from "../images/lake.jpg";
 
-const imageAltText = "purple and blue abstract background";
+const imageAltText = "Picture of a beautiful lake";
 
 /**
  * Sort description that expands on your title on the Home component.
  */
 const description =
-  "I'm a UI/UX student studying at Barnett Technical University. I enjoy creating unique and simplistic user interfaces in creative ways.";
+  "I'm an aspiring software engineer at Barnett Technical University with a passion for data science, UX/UI design, and mobile app development.";
 
 /**
  * List of some of skills or technologies you work on, are learning,
  * passionate about, or enjoy,
  */
 const skillsList = [
-  "Web design",
-  "User experience",
-  "Inclusive design",
-  "Focus group testing",
-  "Mobile user interfaces",
-  "Graphic design",
+  "Aspiring Software Engineer",
+  "Data Science Enthusiast",
+  "UX/UI Designing",
+  "Mobile Application Development",
+  "Mobile user interfaces"
 ];
 
 /**
