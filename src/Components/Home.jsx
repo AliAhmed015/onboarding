849288cd6787a8925d1b4e7cbba2ac0dae8ee6ae
AliAhmed015 <114981587+AliAhmed015@users.noreply.github.com>
2024-08-30 18:30:56 +0000

    changes

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -20,7 +20,7 @@ import PropTypes from "prop-types";
  * Need an image? Check out https://unsplash.com to download a photo you
  * freely use on your site.
  */
-import image from "../images/hut.jpg";
+import image from "../images/cube.png";
 
 const imageAltText = "Muhammad Ali Ahmed - Aspiring Software Engineer. This is a background photo of the hut.";
 
